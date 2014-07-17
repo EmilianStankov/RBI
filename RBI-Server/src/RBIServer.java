0c9460f88493df88711d328df377140dbceefbc6
EmilianStankov <eminstine@abv.bg>
2014-07-17 16:26:33 +0300

    Server is done

--- RBI-Server/src/RBIServer.java
+++ RBI-Server/src/RBIServer.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 import com.hackbulgaria.corejava.RBIProtocol;
 
@@ -12,26 +14,41 @@ public class RBIServer {
         long startTime;
         long endTime;
         long executionTime;
-
         try (ServerSocket serverSocket = new ServerSocket(portNumber); Socket clientSocket = serverSocket.accept();) {
-            Runtime process = Runtime.getRuntime();
+            Runtime runtime = Runtime.getRuntime();
             String message;
             while (true) {
-                startTime = System.currentTimeMillis();
-                message = RBIProtocol.readMessage(clientSocket);
-                System.out.println(message);
-                // Process p = process.exec(message);
-                // PrintWriter processOutput = new
-                // PrintWriter(p.getOutputStream());
-                // endTime = System.currentTimeMillis();
-                // executionTime = endTime - startTime;
-                RBIProtocol.writeMessage(clientSocket, String.format("{%s}", message));
-                // System.out.println(executionTime);
+                try {
+                    message = RBIProtocol.readMessage(clientSocket);
+                    startTime = System.currentTimeMillis();
+                    System.out.println(message);
+                    Process p = runtime.exec(message);
+                    p.waitFor();
+                    InputStream processOutput = p.getInputStream();
+                    Scanner scanner = new Scanner(processOutput);
+                    String line = "";
+                    StringBuilder builder = new StringBuilder();
+                    while (scanner.hasNextLine()) {
+                        line = scanner.nextLine();
+                        builder.append(line);
+                    }
+                    System.out.println(builder);
+                    endTime = System.currentTimeMillis();
+                    executionTime = endTime - startTime;
+                    System.out.println(String.format("Command executed in {%d} milliseconds!", executionTime));
+                    RBIProtocol.writeMessage(clientSocket, String.format(
+                            "{%s} executed in {%d} milliseconds, with result: {%s}", message, executionTime, builder));
+                } catch (IOException e) {
+                    RBIProtocol.writeMessage(clientSocket, "Invalid command!");
+                }
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port " + portNumber
                     + " or listening for a connection");
             System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
